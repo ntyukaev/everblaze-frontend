7f862f929cc8db7c6688bf6f75d4c7b900a1079d
Nikolay Tyukaev <ntyukaev@gmail.com>
2022-07-01 16:39:01 +0300

    Squashed commit of the following:
    commit a7b4cc8b01a583bf73ff919a96f21e544906c866
    Author: Nikolay Tyukaev <ntyukaev@gmail.com>
    Date:   Fri Jul 1 16:37:34 2022 +0300
    
        prepare fresh app

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off'
+  }
+}
