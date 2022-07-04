3537f730d8926116df06ca589ca1ffd86095ae88
Nikolay Tyukaev <ntyukaev@gmail.com>
2022-07-04 21:33:33 +0300

    Squashed commit of the following:
    commit 4ab90775b767d4ffdb37bf2077a042709c33d40e
    Author: Nikolay Tyukaev <ntyukaev@gmail.com>
    Date:   Mon Jul 4 21:33:22 2022 +0300
    
        edit view toolbar

--- src/components/ViewToolbar/ViewToolbar.js
+++ src/components/ViewToolbar/ViewToolbar.js
@@ -1,8 +1,16 @@
+import { Button } from 'antd'
+import { BarChartOutlined, TableOutlined, ClusterOutlined } from '@ant-design/icons'
 import styles from './ViewToolbar.module.scss'
 
 const ViewToolbar = () => {
   return (
-    <div className={styles.ViewToolbar}></div>
+    <div className={styles.ViewToolbar}>
+      <div className={styles.ViewList}>
+        <Button type='text' icon={<BarChartOutlined/>} block />
+        <Button type='text' icon={<TableOutlined/>} block />
+        <Button type='text' icon={<ClusterOutlined/>} block />
+      </div>
+    </div>
   )
 }
 
