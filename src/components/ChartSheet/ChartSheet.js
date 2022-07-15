0dfd6a1286e52567779fd96566e389ccda7b8fde
Nikolay Tyukaev <ntyukaev@gmail.com>
2022-07-15 16:17:03 +0300

    Squashed commit of the following:
    commit cd594115e272f14ee6eeec6eecd24c21fe1dfb18
    Author: Nikolay Tyukaev <ntyukaev@gmail.com>
    Date:   Fri Jul 15 16:16:49 2022 +0300
    
        use draggable resizable

--- src/components/ChartSheet/ChartSheet.js
+++ src/components/ChartSheet/ChartSheet.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
+import { Rnd } from 'react-rnd'
 import styles from './ChartSheet.module.scss'
 
 const ChartSheetContainer = styled.div.attrs(({ scale }) => ({
@@ -11,7 +12,20 @@ const ChartSheetContainer = styled.div.attrs(({ scale }) => ({
 const ChartSheet = () => {
   const scale = useSelector((state) => state.sheetScale.scale)
   return (
-    <ChartSheetContainer scale={scale} className={styles.ChartSheet} />
+    <ChartSheetContainer scale={scale} className={styles.ChartSheet}>
+      <Rnd
+        className={styles.Rnd}
+        bounds='parent'
+        default={{
+          x: 0,
+          y: 0,
+          width: '30%',
+          height: '30%'
+        }}
+      >
+        Rnd
+      </Rnd>
+    </ChartSheetContainer>
   )
 }
 
