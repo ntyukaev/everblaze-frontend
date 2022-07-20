9456f789de33780f66fee36b7c49de4cd9eefb03
Nikolay Tyukaev <ntyukaev@gmail.com>
2022-07-21 01:30:15 +0300

    Refactor Report view

--- src/components/View/Report/Report.js
+++ src/components/View/Report/Report.js
@@ -1,18 +1,67 @@
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { Spin } from 'antd'
+import { TopMenu, Playground, TabList, ViewToolbar, BottomInfo, RightSidebar } from '../../Layout'
 import ReportLoader from './ReportLoader'
-import { TopMenu, ViewToolbar, BottomInfo } from '../../Layout'
 import ScaleSlider from '../../ScaleSlider'
-import ReportPlayground from './ReportPlayground'
+import ReportArea from './ReportArea'
+import SheetList from './SheetList'
+import { useSheets } from '../../../hooks'
+import { selectedSheetsVar } from '../../../apollo'
+import { writeSheet } from '../../../operations'
+import styles from './Report.module.scss'
 
 const Report = ({ id, name, selectedSheet }) => {
+  const { error, loading, data } = useSheets(+id)
+  useEffect(() => {
+    if (!selectedSheet) {
+      if (data?.sheets) {
+        if (data.sheets.length > 0) {
+          selectedSheetsVar({
+            ...selectedSheetsVar(),
+            [id]: data.sheets[0].id
+          })
+        } else {
+          writeSheet({ index: 0, id: 10003, name: 'New Sheet' }, { id })
+        }
+      }
+    }
+  }, [data, selectedSheet])
+
+  if (error) {
+    return (
+      <div>An error occured</div>
+    )
+  }
+  if (loading || !selectedSheet) {
+    return (
+      <div className={styles.Report}>
+        <Spin />
+      </div>
+    )
+  }
+
   return (
     <>
+      <Playground>
+        <Playground.Body>
+          <Playground.Canvas>
+            <ReportArea selectedSheet={selectedSheet} />
+            <TabList>
+              <SheetList selectedSheet={selectedSheet} sheets={data.sheets} />
+            </TabList>
+          </Playground.Canvas>
+          <Playground.Sidebars>
+            <RightSidebar key='Visualizations' title="Visualizations" />
+            <RightSidebar key="Fields" title="Fields" />
+          </Playground.Sidebars>
+        </Playground.Body>
+      </Playground>
       <TopMenu reportName={name} />
       <ViewToolbar />
       <BottomInfo>
         <ScaleSlider />
       </BottomInfo>
-      <ReportPlayground reportId={id} selectedSheet={selectedSheet}/>
     </>
   )
 }
--- src/components/View/Report/ReportPlayground/index.js
+++
@@ -1,3 +0,0 @@
-import ReportPlayground from './ReportPlayground'
-
-export default ReportPlayground
