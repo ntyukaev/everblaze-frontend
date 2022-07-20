fca154b0d09ddb894e4c937f63f9e37a4e78d011
Nikolay Tyukaev <ntyukaev@gmail.com>
2022-07-20 22:49:26 +0300

    delete AppRoutes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,29 @@
-import AppRoutes from '../AppRoutes'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from '../../contexts/useAuth'
+import ReportView from '../ReportView'
+import DataView from '../DataView'
+import ModelView from '../ModelView'
+import Register from '../Register'
+import Login from '../Login'
 import 'antd/dist/antd.min.css'
 import './App.scss'
 
 const App = () => {
   return (
-    <AppRoutes />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='app/:reportId'>
+            <Route path='report' element={<ReportView />} />
+            <Route path='data' element={<DataView />} />
+            <Route path='model' element={<ModelView />} />
+          </Route>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<Navigate to='/choose_report' />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
